944d17877a095ec4b80e6c98c9c31b979e7541ec
ThisIsNotJacob <thisisnotjacob@163.com>
2019-08-29 10:11:22 +0800

    完成借模块输入

--- src/pages/voucher/addVoucher/voucherList.vue
+++ src/pages/voucher/addVoucher/voucherList.vue
@@ -7,6 +7,7 @@
               v-model="state1"
               :fetch-suggestions="querySearch"
               placeholder=""
+              :trigger-on-focus="false"
               @select="handleSelect"
             ></el-autocomplete>
           </div>
@@ -20,13 +21,43 @@
             ></el-autocomplete>
           </div>
           <div class="borrow">
-            <div class="borrow-bottom">
-              <div v-for="(item,index) in 13" :key="index"></div>
+            <div class="borrow-bottom" @click="borrowClick">
+              <div class="div-box" v-show="!borrowInput">
+                <div>{{borrowCountArray[12] == undefined ? '' : borrowCountArray[12]}}</div>
+                <div>{{borrowCountArray[11] == undefined ? '' : borrowCountArray[11]}}</div>
+                <div>{{borrowCountArray[10] == undefined ? '' : borrowCountArray[10]}}</div>
+                <div>{{borrowCountArray[9] == undefined ? '' : borrowCountArray[9]}}</div>
+                <div>{{borrowCountArray[8] == undefined ? '' : borrowCountArray[8]}}</div>
+                <div>{{borrowCountArray[7] == undefined ? '' : borrowCountArray[7]}}</div>
+                <div>{{borrowCountArray[6] == undefined ? '' : borrowCountArray[6]}}</div>
+                <div>{{borrowCountArray[5] == undefined ? '' : borrowCountArray[5]}}</div>
+                <div>{{borrowCountArray[4] == undefined ? '' : borrowCountArray[4]}}</div>
+                <div>{{borrowCountArray[3] == undefined ? '' : borrowCountArray[3]}}</div>
+                <div>{{borrowCountArray[2] == undefined ? '' : borrowCountArray[2]}}</div>
+                <div>{{borrowCountArray[1] == undefined ? '' : borrowCountArray[1]}}</div>
+                <div>{{borrowCountArray[0] == undefined ? '' : borrowCountArray[0]}}</div>
+              </div>
+              <input type="text" ref="gain" v-show="borrowInput" @input="borrowInputEvent" @blur="borrowBlurEvent">
             </div>
           </div>
           <div class="loan">
             <div class="loan-bottom">
-              <div v-for="(item,index) in 13" :key="index"></div>
+              <div class="div-box">
+                <div>{{borrowCountArray[12] == undefined ? '' : borrowCountArray[12]}}</div>
+                <div>{{borrowCountArray[11] == undefined ? '' : borrowCountArray[11]}}</div>
+                <div>{{borrowCountArray[10] == undefined ? '' : borrowCountArray[10]}}</div>
+                <div>{{borrowCountArray[9] == undefined ? '' : borrowCountArray[9]}}</div>
+                <div>{{borrowCountArray[8] == undefined ? '' : borrowCountArray[8]}}</div>
+                <div>{{borrowCountArray[7] == undefined ? '' : borrowCountArray[7]}}</div>
+                <div>{{borrowCountArray[6] == undefined ? '' : borrowCountArray[6]}}</div>
+                <div>{{borrowCountArray[5] == undefined ? '' : borrowCountArray[5]}}</div>
+                <div>{{borrowCountArray[4] == undefined ? '' : borrowCountArray[4]}}</div>
+                <div>{{borrowCountArray[3] == undefined ? '' : borrowCountArray[3]}}</div>
+                <div>{{borrowCountArray[2] == undefined ? '' : borrowCountArray[2]}}</div>
+                <div>{{borrowCountArray[1] == undefined ? '' : borrowCountArray[1]}}</div>
+                <div>{{borrowCountArray[0] == undefined ? '' : borrowCountArray[0]}}</div>
+              </div>
+              <input type="text">
             </div>
           </div>
         </div>
@@ -35,9 +66,12 @@
 export default {
   data() {
     return {
+        borrowInput: false,
         restaurants: [],
         state1: '',
         state2: '',
+        autocomplete: true,
+        borrowCountArray: []
     }
   },
   methods:{
@@ -68,6 +102,23 @@ export default {
       },
       handleSelect(item) {
         console.log(item);
+      },
+      borrowClick() {
+        this.borrowInput = true;
+        this.$nextTick(()=> {
+          this.$refs.gain.focus()
+        })
+      },
+      borrowInputEvent() {
+        let borrowValue = this.$refs.gain.value + '';
+        if(borrowValue.indexOf('.') == -1) {
+          this.borrowCountArray = (borrowValue + '00').split('').reverse()
+          console.log(this.borrowCountArray)
+        }
+      },
+      borrowBlurEvent() {
+        this.$refs.gain.value = null
+        this.borrowInput = false
       }
     },
     mounted() {
@@ -128,15 +179,27 @@ export default {
           display: flex;
           justify-content: center;
           align-items: center;
-          div {
+          .div-box {
             flex: 1;
-            height: 59px;
-            border-right: 1px solid #666666;
-            box-sizing: border-box;
+            display: flex;
+            div {
+              flex: 1;
+              height: 60px;
+              border-right: 1px solid #666666;
+              box-sizing: border-box;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            }
+            div:last-child {
+              border-right: none;
+            }
           }
-          div:last-child {
-            border-right: none;
-
+          input {
+            flex: 1;
+            height: 58px;
+            border: 1px solid #666666;
+            outline: none
           }
         }
       }
